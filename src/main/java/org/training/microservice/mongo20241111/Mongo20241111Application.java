21e16e4316b291acb326ba4b1533426e663c903f
osman yaycioglu <osman.yaycioglu@gmail.com>
2024-11-11 11:40:02 +0300

    Initial commit

---
+++ src/main/java/org/training/microservice/mongo20241111/Mongo20241111Application.java
@@ -0,0 +1,14 @@
+package org.training.microservice.mongo20241111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mongo20241111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mongo20241111Application.class,
+                              args);
+    }
+
+}
---
+++ src/test/java/org/training/microservice/mongo20241111/Mongo20241111ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.training.microservice.mongo20241111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mongo20241111ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
